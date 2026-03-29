4cd2dec8b93a44b2a9f93c545b86b5b7e1d5d369
volter89 <tomasz.jeszke89@gmail.com>
2026-03-29 19:22:51 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class RequestData(BaseModel):
+    start: str
+    end: str
+    ride_time: int
+    transfer_time: int
+    total_time: int
+
+@app.get("/")
+def home():
+    return {"status": "ZTM backend działa 🚍"}
+
+@app.post("/plan")
+def plan(data: RequestData):
+    return {
+        "route": [
+            f"🚍 Start: {data.start}",
+            f"➡️ Jedź około {data.ride_time} min",
+            f"🔁 Przesiadka {data.transfer_time} min",
+            f"🏁 Koniec: {data.end}"
+        ],
+        "total_time": data.total_time
+    }
