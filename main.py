358b847c96919c04ee54b1b5f20f5a1544791af5
volter89 <tomasz.jeszke89@gmail.com>
2026-04-23 09:47:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -116,10 +116,7 @@ def plan(data: RequestData):
 
                 wait = dep - current_time
 
-                if wait < 2:
-                    continue
-
-                if wait > data.transfer_time:
+                if wait < data.transfer_time:
                     continue
 
                 for j in range(i+2, min(i+8, len(stops))):
